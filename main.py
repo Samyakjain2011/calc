8f79ea1cdc4ba15ee0aefee5a3fe2c29cd5a973a
Samyakjain2011 <depakjain@gmail.com>
2024-12-31 16:36:15 +0530

    3rd commit

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-choice=input('''enter what operation you need to use
-             1. +
+while True:
+
+    choice=input('''enter what operation you need to use
+                1. +
              2. -
              3. *
              4. /
              ''')
-n1=int(input("enter the first number"))
-n2=int(input("enter the second number"))
-if choice=="1":
-    print("the ans is",n1+n2)
-elif choice=="2":
-    print("ans is", n1-n2)
-elif choice=="3":
-    print("the ans is", n1*n2)
-elif choice=="4":
-    print("the ans is", n1/n2)
+
+    n1=int(input("enter the first number"))
+    n2=int(input("enter the second number"))
+    if choice=="1":
+        print("the ans is",n1+n2)
+    elif choice=="2":
+        print("ans is", n1-n2)
+    elif choice=="3":
+        print("the ans is", n1*n2)
+    elif choice=="4":
+        print("the ans is", n1/n2)
